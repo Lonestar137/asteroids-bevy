cd321598e9dd65919ef24cfd44712ae1061afd77
Garrett Jones <garretcj2010@hotmail.com>
2024-01-17 17:09:30 -0600

    changes to reflect performance updates

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,7 @@
 pub const WW: i32 = 1920;
 pub const WH: i32 = 1080;
+// pub const WW: i32 = 2560;
+// pub const WH: i32 = 1440;
 // x coordinates
 pub const LEFT_WALL: f32 = -(WW / 2) as f32;
 pub const RIGHT_WALL: f32 = (WW / 2) as f32;
@@ -7,4 +9,4 @@ pub const RIGHT_WALL: f32 = (WW / 2) as f32;
 pub const BOTTOM_WALL: f32 = -(WH / 2) as f32;
 pub const TOP_WALL: f32 = (WH / 2) as f32;
 pub const BG_COLOR: (u8, u8, u8) = (0, 0, 0);
-pub const BASE_EXP_PULL: f32 = 70.;
+pub const BASE_EXP_PULL: f32 = 250.;
